dcfef5f9f3de07fcb77ab31596b26c6601c7f576
Roshan <rs123456raushan@gmail.com>
2021-04-01 14:25:06 +0530

    first commit

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots=[
+ {
+ 	id :1,
+ 	name: 'Rahul',
+ 	username: 'itsme',
+ 	email: 'rahulk@gmail.com'
+ },
+  {
+ 	id :2,
+ 	name: 'Sandeep',
+ 	username: 'buddy',
+ 	email: 'sandeepk@gmail.com'
+ },
+  {
+ 	id :3,
+ 	name: 'Jyoti',
+ 	username: 'liljk',
+ 	email: 'jyotik@gmail.com'
+ },
+  {
+ 	id :4,
+ 	name: 'Pavan',
+ 	username: 'clgfrnd',
+ 	email: 'pavank@gmail.com'
+ },
+  {
+ 	id :5,
+ 	name: 'Raja',
+ 	username: 'bstbdy',
+ 	email: 'rajak@gmail.com'
+ },
+  {
+ 	id :6,
+ 	name: 'Pawan',
+ 	username: 'clgbudy',
+ 	email: 'pwnk@gmail.com'
+ },
+  {
+ 	id :7,
+ 	name: 'Krishna',
+ 	username: 'plyboy',
+ 	email: 'krishnak@gmail.com'
+ },
+  {
+ 	id :8,
+ 	name: 'Yogendra',
+ 	username: 'incntbudy',
+ 	email: 'ygnk@gmail.com'
+ },
+  {
+ 	id :9,
+ 	name: 'Pradeep',
+ 	username: 'skhtlonda',
+ 	email: 'rahulk@gmail.com'
+ },
+  {
+ 	id :10,
+ 	name: 'Roshan',
+ 	username: 'itsrossh',
+ 	email: 'rosshk@gmail.com'
+ }
+ 
+]
